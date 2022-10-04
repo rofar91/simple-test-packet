d07fb20a2800c3baa28a01bde81d8c49785350ae
Dima Abramovich <abramovich_d@rocketdata.io>
2022-10-04 18:16:18 +0300

    Add start uvicorn script

--- simple_test_packet/main.py
+++ simple_test_packet/main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,3 +9,8 @@ app = FastAPI()
 @app.get("/")
 def read_root(name: Optional[str] = None):
     return {"Hello": name}
+
+
+def start():
+    """Launched with `poetry run start` at root level"""
+    uvicorn.run("simple_test_packet.main:app", host="0.0.0.0", port=8000, reload=True)

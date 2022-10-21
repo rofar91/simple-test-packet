4c6e01a51418ef717af1aa2b4f3e7cb34a90938d
Dima Abramovich <abramovich_d@rocketdata.io>
2022-10-21 16:46:09 +0300

    Add post endpoint

--- simple_test_packet/main.py
+++ simple_test_packet/main.py
@@ -1,7 +1,8 @@
-from typing import Optional
+from typing import Optional, Union
 
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,6 +10,16 @@ app = FastAPI()
 @app.get("/")
 def read_root(name: Optional[str] = None):
     return {"Hello": name}
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+
+
+@app.post("/name")
+def create_item(item: Item):
+    return item
 
 
 def start():

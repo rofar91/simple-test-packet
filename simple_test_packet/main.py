7aca05aea939b9d22cc91ba6ce2ce57c23e9dd86
Dima Abramovich <abramovich_d@rocketdata.io>
2022-11-19 11:55:06 +0300

    Add endpoint to get key value from Redis

--- simple_test_packet/main.py
+++ simple_test_packet/main.py
@@ -1,10 +1,13 @@
-from typing import Optional, Union
+from typing import Optional
 
 import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+from simple_test_packet.config import redis_connect
+from simple_test_packet.models import Item
 
 app = FastAPI()
+client = redis_connect()
 
 
 @app.get("/")
@@ -12,9 +15,12 @@ def read_root(name: Optional[str] = None):
     return {"Hello": name}
 
 
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
+@app.get("/redis")
+def get_redis_key(key: Optional[str] = None):
+    if key:
+        value = client.get(key)
+        return {key: value}
+    return 'Укажите ?key='
 
 
 @app.post("/name")
